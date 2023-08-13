ed14cbb44edde09de330ceceee12c2a47cf50eff
kirillbrujah <kirillbrujah@gmail.com>
2023-08-13 02:02:24 +0200

    Add localization to login errors. Fix custom button. Rework localization resource file

--- src/app/custom-button/custom-button.component.ts
+++ src/app/custom-button/custom-button.component.ts
@@ -1,12 +1,16 @@
 import { Component, Input } from '@angular/core';
 
+
+type ButtonTypes = 'submit' | 'button';
 @Component({
   selector: 'app-custom-button',
   templateUrl: './custom-button.component.html',
   styleUrls: ['./custom-button.component.css']
 })
 export class CustomButtonComponent {
-  @Input() isLoading: boolean = false;
+  @Input() loading: boolean = false;
+  @Input() disabled: boolean = false;
   @Input({ required: true }) label!: string;
-  @Input({ required: true }) click!: VoidFunction;
+  @Input() type!: ButtonTypes;
+  @Input() click!: VoidFunction;
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, LOCALE_ID, inject } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { Router } from '@angular/router';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-
+import '@angular/localize/init';
 
 
 class LoginModel {
@@ -46,47 +46,37 @@ export class LoginComponent {
     password: new FormControl('', { nonNullable: true, validators: Validators.required }),
   });
 
+
+  // name= new FormControl('', {
+  //   nonNullable: true,
+  //   validators: Validators.compose([Validators.required, Validators.minLength(4)]),
+  // });
+  // password= new FormControl('', { nonNullable: true, validators: Validators.required });
+
+
   // loginForm = new FormGroup<LoginForm>({
   //   name: new FormControl('', {nonNullable: true, validators: Validators.required}),
   //   password: new FormControl('', {nonNullable: true, validators: Validators.required}), 
   // });
 
   getErrorMessage(): string {
-    // console.log(this.loginModel);
-    // console.log({ 
-    //   loginModel: this.loginModel,
-    //   nameErrors: this.name.errors,
-    //   passwrodErrors: this.password.errors,
-    // });
-    console.log(this.loginForm.errors);
-    // console.log(this.password.errors);
-    // if (this.loginForm.hasError('required')) {
-    //   return 'REQUI';
-    // }
-
-    return "ERR";
+    // console.log(this.loginForm.errors);
+    // console.log(this.loginForm.get('name')?.errors);
+    // console.log(this.loginForm.get('password')?.errors);
+    // return '2';
+    return $localize`Field is required`;
   }
 
 
   constructor(public authService: AuthService, private _router: Router) {
-    // this.isLogged = authService.isAuthenticated();
   }
 
-  login(/* name: string, password: string */): VoidFunction {
-    // console.log(this.loginForm.value);
-    console.log(this.loginModel);
-
-    return () => {
-      // console.log(this.loginForm.value);
-      // return;
-
-      this.loginLoading = true;
-      this.authService.login().then(() => {
-        console.log("IN THEN");
-        this.loginLoading = false;
-        // this._router.navigate(["/"], {replaceUrl: true});      
-      });
-      // this.isLogged = this.authService.isAuthenticated();
-    };
+  async login(/* name: string, password: string */) {
+    this.loginLoading = true;
+    this.authService.login().then(() => {
+      console.log("IN THEN");
+      this.loginLoading = false;
+      this._router.navigate(["/"], {replaceUrl: true});      
+    });
   }
 }
